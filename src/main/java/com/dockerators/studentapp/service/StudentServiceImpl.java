b3618fac74a1897d5d29878d0134325c367b1acf
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-07-02 17:18:12 +0530

    Added Logging Functionality

--- src/main/java/com/dockerators/studentapp/service/StudentServiceImpl.java
+++ src/main/java/com/dockerators/studentapp/service/StudentServiceImpl.java
@@ -5,6 +5,8 @@ import com.dockerators.studentapp.entity.Student;
 import com.dockerators.studentapp.exception.NullFieldsException;
 import com.dockerators.studentapp.exception.StudentAlreadyExistsException;
 import com.dockerators.studentapp.exception.StudentNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ import java.util.Optional;
 public class StudentServiceImpl implements StudentService {
     // Private repository to access the student database and perform SQL Queries
     private StudentRepository studentRepository;
+    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
 
     @Autowired
     // Constructor
@@ -25,6 +28,7 @@ public class StudentServiceImpl implements StudentService {
     @Override
     // Will return a list of all students in the database
     public List<Student> findAll() {
+        logger.info("Retrieving All Students");
         return (this.studentRepository.findAll());
     }
 
@@ -36,9 +40,11 @@ public class StudentServiceImpl implements StudentService {
         Student student;
         if(result.isPresent()) {
             student = result.get();
+            logger.info(String.format("Student with Id %s was retrieved.", id));
             return(student);
         }else{
             // Will throw error if a student with that ID is not found
+            logger.error(String.format("Student with Id %s was not found.", id));
             throw new StudentNotFoundException();
         }
     }
@@ -48,12 +54,15 @@ public class StudentServiceImpl implements StudentService {
         try{
             Optional <Student> result = this.studentRepository.findByRollNo(student.getRollNo());
             if(result.isEmpty()){
-                // Will throw error if a student with that ID is not found
+                // Will throw error if a student with that roll number is not found
                 throw new StudentNotFoundException();
             }
             student.setId(result.get().getId());
-            return this.studentRepository.save(student);
+            this.studentRepository.save(student);
+            logger.info(String.format("Student with Roll No %s was updated.", student.getRollNo()));
+            return student;
         } catch (StudentNotFoundException e) {
+            logger.error(String.format("Student with Roll No %s does not exist and hence can't be updated.", student.getRollNo()));
             throw e;
         } catch (RuntimeException e){
             // Will throw error if any of the fields are empty.
@@ -69,10 +78,13 @@ public class StudentServiceImpl implements StudentService {
             if(s.isPresent()){
                 throw new StudentAlreadyExistsException();
             }else{
-                return (this.studentRepository.save(student));
+                this.studentRepository.save(student);
+                logger.info(String.format("Student with Roll No %s was added.", student.getRollNo()));
+                return student;
             }
         }catch (StudentAlreadyExistsException e){
             // Will throw error if a student with that Roll Number already exists
+            logger.error(String.format("Student with Roll No %s already exists and hence can't be added.", student.getRollNo()));
             throw e;
         }catch (RuntimeException e){
             // Will throw error if any of the fields are NULL.
@@ -87,9 +99,11 @@ public class StudentServiceImpl implements StudentService {
         Optional<Student> b = this.studentRepository.findById(id);
         if (b.isEmpty()) {
             // Will throw error if a student with that ID does not exist
+            logger.error(String.format("Student with Id %s doesn't exist and hence can't be deleted.", id));
             throw new StudentNotFoundException();
         } else {
             this.studentRepository.deleteById(id);
+            logger.info(String.format("Student with Id %s has been deleted.", id));
             return b.get();
         }
     }
